8ed3b4c334060f1b97156b650e4db62305efe208
Arham-Amir <arhamamir424@gmail.com>
2024-07-30 12:25:57 -0700

    Added Google tag manager

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,6 +11,7 @@ import { ModeToggle } from "@/components/ui/toggleModeButton";
 import Whatsapp from "@/components/ui/Whatsapp";
 import Testimonials from "@/components/Testimonials";
 import Headline from "@/components/Headline";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,11 +27,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+
+        <Script
+        id="gtm"
+        strategy="afterInteractive" 
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-59G7GXVL');`
+        }}>
+        </Script>
+
+      </head>
       <body className={`${inter.className} overflow-x-hidden`}>
         <ThemeProvider attribute="class" defaultTheme="system">
           <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
             <ToasterProvider />
-            <Headline />
+            {/* <Headline /> */}
             <Navbar />
             <main className="">
               {children}
