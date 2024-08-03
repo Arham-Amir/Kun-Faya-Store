116e1cf50069aca992a9d6389b83c479e83568af
Arham-Amir <arhamamir424@gmail.com>
2024-08-03 13:29:17 -0700

    update related products

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -29,7 +29,7 @@ const ProductDetails = async ({ params }: { params: { productId: string } }) => 
       {relatedProducts.length != 0 &&
         <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
           <p className="text-heading3-bold">Related Products</p>
-          <div className="flex flex-wrap gap-16 mx-auto mt-8">
+          <div className="flex justify-center items-center flex-wrap gap-16 mx-auto mt-8">
             {relatedProducts?.map((product: ProductType) => (
               <AnimatedCard key={product._id} product={product} />
             ))}
